6e2fb8eb0639a55035afea1073c1ca9845bbf388
djkabutar <d.kabutarwala@yahoo.com>
2024-07-16 11:04:30 +0530

    pyrah: making internal functions private
    Signed-off-by: djkabutar <d.kabutarwala@yahoo.com>

--- pyrah/__init__.py
+++ pyrah/__init__.py
@@ -7,23 +7,23 @@ LIBNAME = "librah.so"
 CLIB = "libc.so.6"
 
 libname = LIBNAME
-rah = ctypes.CDLL(libname)
-c = ctypes.CDLL(CLIB)
+__rah = ctypes.CDLL(libname)
+__c = ctypes.CDLL(CLIB)
 
-def get_buffer(appid, size):
-    rah.rah_request_mem.restype = ctypes.c_ulong
-    buffer = rah.rah_request_mem(appid, size)
+def __get_buffer(appid, size):
+    __rah.rah_request_mem.restype = ctypes.c_ulong
+    buffer = __rah.rah_request_mem(appid, size)
     return buffer
 
-def remove_buffer(buffer):
-    return rah.rah_free_mem(buffer)
+def __remove_buffer(buffer):
+    return __rah.rah_free_mem(buffer)
 
-def write_buffer(appid, buffer, d_len):
-    rah.rah_write_mem.argtypes = [ctypes.c_ubyte, ctypes.c_char_p, ctypes.c_ulong]
-    rah.rah_write_mem(appid, buffer, d_len)
+def __write_buffer(appid, buffer, d_len):
+    __rah.rah_write_mem.argtypes = [ctypes.c_ubyte, ctypes.c_char_p, ctypes.c_ulong]
+    __rah.rah_write_mem(appid, buffer, d_len)
 
 def get_max_buffer_size():
-    return rah.rah_max_buffer_size()
+    return __rah.rah_max_buffer_size()
 
 def rah_write(appid, d_buf):
     if type(d_buf) != bytes:
@@ -34,18 +34,18 @@ def rah_write(appid, d_buf):
         raise Exception("Maximum Buffer size should be " +
                 str(meximum_buffer_size) + "!")
 
-    buffer = get_buffer(appid, len(d_buf))
+    buffer = __get_buffer(appid, len(d_buf))
     buf_loc = ctypes.c_char_p(buffer)
 
     data = np.array(d_buf).ctypes.data_as(ctypes.c_char_p)
-    c.memcpy(buf_loc, data, len(d_buf))
+    __c.memcpy(buf_loc, data, len(d_buf))
 
-    write_buffer(appid, buf_loc, len(d_buf))
-    remove_buffer(buffer)
+    __write_buffer(appid, buf_loc, len(d_buf))
+    __remove_buffer(buf_loc)
 
 def rah_read(appid, d_len):
     ptr = ctypes.create_string_buffer(d_len)
-    rah.rah_read.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_ulong]
+    __rah.rah_read.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_ulong]
 
-    rah.rah_read(appid, ptr, d_len)
+    __rah.rah_read(appid, ptr, d_len)
     return repr(ptr.raw)
