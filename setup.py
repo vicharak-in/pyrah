0c38037d774a238f2ecfd8413b4d06b36fea2e4a
djkabutar <d.kabutarwala@yahoo.com>
2025-03-24 10:25:55 +0530

    setup: bump the version to 0.0.4
    Signed-off-by: djkabutar <d.kabutarwala@yahoo.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = "pyrah",
     packages = find_packages(),
-    version = "0.0.3",
+    version = "0.0.4",
     install_requires = ['numpy'],
     description = "Vaaman Read - Write functions for FPGA",
     author = "djkabutar <d.kabutarwala@yahoo.com>",
